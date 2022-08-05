624467003ba90681d2f14e03f63e0ad70f78166a
proguy18 <josephjoey.leonardi@gmail.com>
2022-08-05 20:19:05 +0700

    Added backend functionalities

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { body, param, validationResult } from "express-validator";
 import cors from "cors";
 import { Request } from "express-serve-static-core";
 import { ParsedQs } from "qs";
@@ -9,50 +10,58 @@ const app = express();
 // const port: number = +(process.env.PORT || 48080);
 const port: number = 48080;
 
-const users: { name: string; password: string; }[] = []
+const users: { name: string; password: string }[] = [];
 
-app.get('/users', (req, res) => {
-  res.json(users)
-})
+// middleware
+app.use(express.json());  
 
-app.post('/users', async (req, res) => {
+app.get("/users", (req: express.Request, res: express.Response) => {
+  res.json(users);
+});
+
+app.post("/users", (req: express.Request, res: express.Response) => {
   try {
-    const user = { name: req.body.name, password: req.body.password }
-    users.push(user)
-    res.status(201).send()
+    const user = { name: req.body.name, password: req.body.password };
+    users.push(user);
+    res.status(201).send();
   } catch {
-    res.status(500).send()
+    res.status(500).send("An error has occured");
   }
-})
+});
 
-app.post('/users/login', async (req, res) => {
-  const user = users.find(user => user.name === req.body.name)
+app.post("/users/login", (req: express.Request, res: express.Response) => {
+  const user = users.find((user) => user.name === req.body.name);
   if (user == null) {
-    return res.status(400).send('Cannot find user')
+    return res.status(400).send("Cannot find user");
   }
   try {
-    if(userAuthenticate(req, user)) {
-      res.send('Success')
+    if (userAuthenticate(req, user)) {
+      res.send("Success");
     } else {
-      res.send('Not Allowed')
+      res.send("Not Allowed");
     }
   } catch {
-    res.status(500).send()
+    res.status(500).send();
   }
-})
+});
 
 /* Listen for incoming connections */
 app.listen(port, () => {
-    console.log(`API listening on port ${port}.`);
+  console.log(`API listening on port ${port}.`);
 });
 
 /* Enable CORS */
-app.use(cors({
+app.use(
+  cors({
     origin: ["http://localhost:3000", "http://localhost:48080"],
     credentials: true,
-    optionsSuccessStatus: 200
-}));
+    optionsSuccessStatus: 200,
+  })
+);
 
-function userAuthenticate(req: Request<{}, any, any, ParsedQs, Record<string, any>>, user: { name: string; password: string; }) {
-    return req.body.password == user.password;
+function userAuthenticate(
+  req: Request<{}, any, any, ParsedQs, Record<string, any>>,
+  user: { name: string; password: string }
+) {
+  return req.body.password == user.password;
 }
